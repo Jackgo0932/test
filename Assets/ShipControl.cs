f4b0fc5d8056cb2a9917bbeeb4241bcdfe314301
Jackgo0932 <jackgo0932@gmail.com>
2017-03-16 20:44:23 +0800

    test
    123

---
+++ Assets/ShipControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShipControl : MonoBehaviour {
+	public GameObject Bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.RightArrow)&&gameObject.transform.position.x<3.5f) {
+			gameObject.transform.position += new Vector3(0.1f,0,0);
+		}
+		if (Input.GetKey (KeyCode.LeftArrow)&&gameObject.transform.position.x>-3.5f) {
+			gameObject.transform.position += new Vector3(-0.1f,0,0);
+		}
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Vector3 pos = gameObject.transform.position + new Vector3(0,1.2f,0);
+			Instantiate(Bullet,pos,gameObject.transform.rotation);
+			}
+	}
+}
